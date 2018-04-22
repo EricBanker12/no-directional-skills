c48c961a85560a73aab8d8fee385ff57e16553ef
Mister-Kay <private-information>
2018-04-22 17:39:11 -0500

    WIP: not functional

--- README.md
+++ README.md
@@ -1,38 +1,13 @@
 # No Directional Skills
+**WIP: This module does not currently work.**
 
-**Bug: Not working for jaunts**
-
-Removes directional input on some skills, makes them work like the old way.
-- Archer: Backstep, Breakaway
-- Priest: Backstep, Fiery
-- Sorcerer: Backstep, Glacial, ~~Jaunt~~
-- Lancer: Backstep
-- ~~Mystic: Jaunt~~
+Removes directional input on some skills, so they always go toward camera target.
 
 See preview: (http://i.imgur.com/nIWqQDZ.mp4)
 
 
 ## How to install
-
-1. Move the two folders "gkm" and "EventEmitter2" from dependencies.zip into "tera-proxy/node_modules". 
-This is where you will find folders like tera-data, etc...
-
-2. Move the folder "no-directional-skills" into "tera-proxy/bin/node_modules".
-This is normally where you put scripts
-
+This module does not currently work.
 
 ## Dependencies:
-
-- GKM, An event based, Global Keyboard and Mouse listener. (https://github.com/tomzx/gkm) 
-- EventEmitter2, an implementation of the EventEmitter module found in Node.js.(https://github.com/asyncly/EventEmitter2)
-
-
-## Changelog 
-### 1.2
-* [+] ~~Added Teleport Jaunt for Sorcerer and Mystic~~
-### 1.1
-* [+] Compatbile with [Skill Prediction](https://github.com/pinkipi/skill-prediction)
-* [+] Works for all skill levels
-
-
-HUGE Special Thanks to [Pinkie Pie](https://github.com/pinkipi) for compatibility and fixes!
+`node-to-ahk` is currently private. This module does not currently work.
--- index.js
+++ index.js
@@ -1,56 +1,56 @@
-//vers 1.2
-
-let gkm = require('gkm');
+const ahk = require('node-to-ahk')
 
 module.exports = function NoDirectionalSkills(dispatch) {
-	let job, hooked, keyW, keyA, keyS, keyD;
+    const skills = require('./config.js')
+    
+    let job = -1,
+        keys = {}
 
-	let skillsList = {
-		1: [ // Lancer
-			26 // Backstep
-		],
-		5: [ // Archer
-			6, // Backstep
-			16 // Breakaway Bolt
-		],
-		6: [ // Priest
-			26, // Fiery Escape
-			38 // Backstep
-		],
-		4: [ // Sorcerer
-			7, // Backstep
-			18, // Glacial Retreat
-			26 // Jaunt
-		],
-		7: [ // Mystic
-			17 // Jaunt
-		]
-	};
+    // get WASD status
+    for (let hotkey of ['w', 'a', 's', 'd']) {
+        keys[hotkey] = false
+        ahk.hook(`~$*${hotkey}`, keyHandler, 'ahk_exe TERA.exe')
+        ahk.hook(`~$*${hotkey} UP`, keyHandler, 'ahk_exe TERA.exe')
+    }
 
-	dispatch.hook('S_LOGIN', 2, (event) => {
-		job = (event.model - 10101) % 100
+    // get WASD status
+    function keyHandler(key) {
+        for (let hotkey of ['w', 'a', 's', 'd']) {
+            if (key.includes(hotkey)) {
+                if (key.includes('UP')) {
+                    keys[hotkey] = false
+                }
+                else {
+                    keys[hotkey] = true
+                }
+            }
+        }
+    }
 
-		if(!hooked && (hooked = true))
-			gkm.events.on('key.*', function(data) {
-				if (data == 'W') keyW = this.event == 'key.pressed';
-				else if (data == 'A') keyA = this.event == 'key.pressed';
-				else if (data == 'S') keyS = this.event == 'key.pressed';
-				else if (data == 'D') keyD = this.event == 'key.pressed';
-			});
-	})
+    // get skill base
+	function skillbase(skill) {
+		return Math.floor((skill - 0x4000000) / 10000)
+	}
 
-	dispatch.hook('C_START_SKILL', 2, (event) => {
-		if(skillsList[job] && skillsList[job].includes(Math.floor((event.skill - 0x4000000) / 10000)) && event.movementkey)
-		{
-			let w = event.w;
+    // get class
+	dispatch.hook('S_LOGIN', 10, (event) => {
+		job = (event.templateId - 10101) % 100
+    })
 
-			if(keyA) w -= keyW ? 0x6000 : keyS ? 0x2000 : 0x4000
-			else if(keyD) w += keyW ? 0x6000 : keyS ? 0x2000 : 0x4000
-			else if(keyW) w += 0x8000;
-
-			event.w = (w & 0xffff) << 16 >> 16;
-			event.movementkey = 0;
-			return true;
-		} 
+    // change direction to camera
+	dispatch.hook('C_START_SKILL', 5, {order: -9999}, (event) => {
+        let skill = skillbase(event.skill)
+        if(skills && skills[job] && skills[job][skill] && event.moving) {
+            if (keys.a && !keys.d && keys.w == keys.s) event.w += Math.PI / 2 // A
+            if (!keys.a && keys.d && keys.w == keys.s) event.w -= Math.PI / 2 // D
+            if (!keys.w && keys.s && keys.a == keys.d) event.w += Math.PI // S
+            if (keys.w && keys.a && !keys.s && !keys.d) event.w += Math.PI / 4 // W+A
+            if (keys.w && !keys.a && !keys.s && keys.d) event.w -= Math.PI / 4 // W+D
+            if (!keys.w && !keys.a && keys.s && keys.d) event.w -= Math.PI * 3 / 4 // S+D
+            if (!keys.w && keys.a && keys.s && !keys.d) event.w += Math.PI * 3 / 4 // S+A
+            if (event.w > Math.PI) event.w -= Math.PI * 2
+            if (event.w < -Math.PI) event.w += Math.PI * 2
+            return true
+        }
 	})
 }
