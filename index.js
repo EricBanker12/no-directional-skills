00984bbdc6ed7c139c60e4d0bad74e9f70333d20
Mister-Kay <private-information>
2018-11-29 09:54:40 -0600

    temporary disable ktera patch 80

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const fs = require('fs'),
 // launch no-directional-skills
 module.exports = function noDirectionalSkillsLauncher(dispatch) {
 
-    /*
-    if (dispatch.base.majorPatchVersion >= 74) {
+    if (dispatch.base.majorPatchVersion > 79) {
         console.log('no-directional-skills - KTera definitions unsupported')
         return
     }
-    */
 
     let extracting = false
 
